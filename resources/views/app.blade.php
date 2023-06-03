14a8edf0328c94a8f96d514f91dfea5c43a37326
francisvirginius <francisvirginius@gmail.com>
2023-06-03 17:23:32 +0200

    Initialisation du projet

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,8 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="/dist/assets/css/app.css">
+    <script type="module" src="/dist/assets/js/app.js" defer></script>
 </head>
 <body>
     <div
